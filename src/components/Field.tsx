3dff46c5ae2bfd2ca75f60b152b89bec651bcc49
Kate Sychenko <busy.sychenko@gmail.com>
2023-07-15 14:41:22 +0400

    Finilize GameOver component

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,43 +1,12 @@
-import { useState } from 'react';
 import Mark from './Mark';
-import GameOver from './GameOver';
 
-const Field = () => {
-  //Declaration of Hooks
-  // Store the board's status
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  //Store the active player status
-  const [activePlayerX, setActivePlayerX] = useState(true);
+interface FieldProps {
+  status: string;
+  squares: string[];
+  handleClick: (index: number) => void;
+}
 
-  //Handle Click funtion - what happens when User clicks on the button
-  function handleClick(index: number) {
-    if (squares[index] || calculateTheWinner(squares)) {
-      return;
-    }
-
-    //create a copy of current status array
-    const nextSquares = squares.slice();
-    //fill in new array with changes based on player's turn
-    nextSquares[index] = activePlayerX == true ? 'X' : 'O';
-    //update the Component's hooks
-    setSquares(nextSquares);
-    setActivePlayerX(!activePlayerX);
-  }
-
-  // Display the status of the game: the turn OR the winner
-  let status;
-  const winner = calculateTheWinner(squares);
-
-  if (winner) {
-    // status = 'Winner is ' + winner;
-    // const transformedWinner = winner.toString();
-    status = 'GAME OVER';
-
-    <GameOver winner='X' />;
-  } else {
-    status = 'Active player is ' + (activePlayerX ? 'X' : '❤️');
-  }
-
+function Field({ status, squares, handleClick }: FieldProps) {
   return (
     <>
       <div className='container'>
@@ -126,34 +95,6 @@ const Field = () => {
       </div>
     </>
   );
-
-  //MARK: как обознать массив массивов??
-  // Game logic
-  // Calculation of winner
-  function calculateTheWinner(squares: number[][]) {
-    const winningCombination = [
-      [0, 1, 2],
-      [0, 4, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 4, 6],
-      [2, 5, 8],
-      [3, 4, 5],
-      [6, 7, 8],
-    ];
-
-    for (let index = 0; index < winningCombination.length; index++) {
-      const [a, b, c] = winningCombination[index];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-};
+}
 
 export default Field;
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,75 @@
+import { useState } from 'react';
 import Field from './Field';
+import GameOver from './GameOver';
+
 function Game() {
+  //Declaration of Hooks
+  // Store the board's status
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //Store the active player status
+  const [activePlayerX, setActivePlayerX] = useState(true);
+
+  //Handle Click funtion - what happens when User clicks on the button
+  function handleClick(index: number) {
+    if (squares[index] || calculateTheWinner(squares)) {
+      return;
+    }
+
+    //create a copy of current status array
+    const nextSquares = squares.slice();
+    //fill in new array with changes based on player's turn
+    nextSquares[index] = activePlayerX == true ? 'X' : 'O';
+    //update the Component's hooks
+    setSquares(nextSquares);
+    setActivePlayerX(!activePlayerX);
+  }
+
+  // Display the status of the game: the turn OR the winner
+  let status;
+  const winner = calculateTheWinner(squares);
+
+  if (winner) {
+    status = 'GAME OVER';
+    return (
+      <>
+        <Field status={status} squares={squares} handleClick={handleClick} />
+        <GameOver winner={winner} />
+      </>
+    );
+  } else {
+    status = 'Active player is ' + (activePlayerX ? 'X' : '❤️');
+  }
+
   return (
     <>
-      <Field />
+      <Field status={status} squares={squares} handleClick={handleClick} />
     </>
   );
+
+  function calculateTheWinner(squares: string[]) {
+    const winningCombination = [
+      [0, 1, 2],
+      [0, 4, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 4, 6],
+      [2, 5, 8],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+
+    for (let index = 0; index < winningCombination.length; index++) {
+      const [a, b, c] = winningCombination[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
 
 export default Game;
